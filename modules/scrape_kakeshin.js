136156a3c9a4897be10dea4b8c810acbb19e98af
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2019-11-28 19:58:37 +0900

    ' → "

--- modules/scrape_kakeshin.js
+++ modules/scrape_kakeshin.js
@@ -2,21 +2,21 @@
  * 掛川信用金庫 WEBバンキングで残高取得
  */
 
-const puppeteer = require('puppeteer');
-const scrape_utils = require('./scrape_utils.js');
+const puppeteer = require("puppeteer");
+const scrape_utils = require("./scrape_utils.js");
 
-const navOption = {'waitUntil':'load'};
+const navOption = {"waitUntil":"load"};
 
 module.exports.scrape = async (page, account) => {
-	process.on('unhandledRejection', console.dir);
+	process.on("unhandledRejection", console.dir);
 	try {
-		await page.goto('https://www11.ib.shinkin-ib.jp/webbk/login/b-prelogin.do?bankcode=MTUxMw==', navOption);
+		await page.goto("https://www11.ib.shinkin-ib.jp/webbk/login/b-prelogin.do?bankcode=MTUxMw==", navOption);
 		// 上記URLからリダイレクトが発生するので、数秒待つ。
 		await page.waitFor(5000);
 
 		// ラポートのインストールを促すダイアログを消す
 		await page.evaluate(() => {
-			let close = document.querySelector('#splash-78561-close-button');
+			let close = document.querySelector("#splash-78561-close-button");
 			if (close != null) {
 				close.click();
 			}
@@ -24,17 +24,17 @@ module.exports.scrape = async (page, account) => {
 
 		// ユーザーIDとパスワードを入力してログイン
 		await page.evaluate((account) => {
-			document.querySelector('input[name=userId]').value = account.user_id;
-			document.querySelector('input[name=loginPwd]').value = account.password;
+			document.querySelector("input[name=userId]").value = account.user_id;
+			document.querySelector("input[name=loginPwd]").value = account.password;
 
-			document.querySelector('input[type=submit][value=ログイン]').click();
+			document.querySelector("input[type=submit][value=ログイン]").click();
 		}, account);
 
 		await page.waitFor(3000);
 
 		// 残高を表示ボタンを押す
 		await page.evaluate(() => {
-			let button = document.querySelector('input[value=残高を表示]');
+			let button = document.querySelector("input[value=残高を表示]");
 			button.click();
 		});
 
@@ -42,7 +42,7 @@ module.exports.scrape = async (page, account) => {
 
 		// 残高取得
 		let value = await page.evaluate(() => {
-			let container = document.querySelector('span.fsB4XL.balance');
+			let container = document.querySelector("span.fsB4XL.balance");
 			if (container == null) {
 				return null;
 			}
@@ -51,18 +51,18 @@ module.exports.scrape = async (page, account) => {
 
 		// ログアウトしないと、次回スムーズにログインできない
 		await page.evaluate(() => {
-			let links = document.querySelectorAll('a');
+			let links = document.querySelectorAll("a");
 			let target = null;
 
 			for (let link of links) {
-				if (link.innerText.includes('ログアウト')) {
+				if (link.innerText.includes("ログアウト")) {
 					target = link;
 					break;
 				}
 			}
 
 			if (target == null) {
-				console.log('ログアウトボタンが見つからない');
+				console.log("ログアウトボタンが見つからない");
 				return;
 			}
 
@@ -71,13 +71,13 @@ module.exports.scrape = async (page, account) => {
 		await page.waitFor(3000);
 
 		if (value == null) {
-			console.log('掛信残高取得失敗');
+			console.log("掛信残高取得失敗");
 			value = 0;
 		}
-		return [{'name' : '残高' , 'amount' : scrape_utils.parsePrice(value)}];
+		return [{"name" : "残高" , "amount" : scrape_utils.parsePrice(value)}];
 
 	} catch (err) {
-		console.log('掛信残高取得失敗');
+		console.log("掛信残高取得失敗");
 		console.log(err);
 		return null;
 	}
