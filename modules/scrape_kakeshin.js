b84eb7de2078b3ca317a53d9bd7a8b961924f72b
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2019-11-28 20:03:58 +0900

    戻り値修正

--- modules/scrape_kakeshin.js
+++ modules/scrape_kakeshin.js
@@ -74,7 +74,10 @@ module.exports.scrape = async (page, account) => {
 			console.log("掛信残高取得失敗");
 			value = 0;
 		}
-		return [{"name" : "残高" , "amount" : scrape_utils.parsePrice(value)}];
+		return [{"account" : account.name
+		, "name" : "残高" 
+		, "group" : account.aggregate[0].group
+		, "amount" : scrape_utils.parsePrice(value)}];
 
 	} catch (err) {
 		console.log("掛信残高取得失敗");
