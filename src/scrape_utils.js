4ea22562b49f94a02e0a9104c935c099680c9d52
vicugna-pacos <50790874+vicugna-pacos@users.noreply.github.com>
2021-10-17 14:33:58 +0900

    描画が終わるのを待つ関数を追加

--- src/scrape_utils.js
+++ src/scrape_utils.js
@@ -48,3 +48,38 @@ module.exports.clickLink = async (page, selector) => {
 	}
 };
 
+/**
+ * ページの描画が終わるのを待つ
+ * @param {*} page 
+ * @param {*} timeout 
+ */
+module.exports.waitTillHTMLRendered = async (page, timeout = 30000) => {
+	const checkDurationMsecs = 1000;  // チェックする間隔(ミリ秒)
+	const minStableSizeIterations = 2;  // ○回チェックしてサイズに変化がなければOKとする
+	const maxChecks = timeout / checkDurationMsecs;
+	let lastHTMLSize = 0;
+	let checkCounts = 1;
+	let countStableSizeIterations = 0;
+
+	await page.waitForSelector("body", {"visible":true});
+  
+	while (checkCounts++ <= maxChecks){
+		let html = await page.content();
+		let currentHTMLSize = html.length; 
+
+		let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+
+		if (lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize) {
+			countStableSizeIterations++;
+		} else {
+			countStableSizeIterations = 0; //reset the counter
+		}
+
+		if (countStableSizeIterations >= minStableSizeIterations) {
+			break;
+		}
+
+		  lastHTMLSize = currentHTMLSize;
+		  await page.waitForTimeout(checkDurationMsecs);
+	}
+};
